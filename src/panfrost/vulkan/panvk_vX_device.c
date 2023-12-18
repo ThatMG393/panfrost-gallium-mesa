aerisarn/mesa-uwp

commit 56be9a55be03cce9925989e7d22696733488e05b

pan/decode: handle more than one panfrost_device
Before this commit, if an application tried to create more than one
panfrost_device, such as piglit's "ext_image_dma_buf_import" tests,
it would result in a crash when running with PAN_MESA_DEBUG=sync or
PAN_MESA_DEBUG=trace.

This commit fixes that by introducing a pandecode_context, which
encapsulates all the information that is being tracked, and thus
avoiding memory conflicts.

Signed-off-by: Italo Nicola <italonicola@collabora.com>
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24144>

src/panfrost/vulkan/panvk_vX_device.c
@@ -77,10 +77,11 @@ panvk_queue_submit_batch(struct panvk_queue *queue, struct panvk_batch *batch,
       }
 
       if (debug & PANVK_DEBUG_TRACE)
-         pandecode_jc(batch->scoreboard.first_job, pdev->gpu_id);
+         pandecode_jc(pdev->decode_ctx, batch->scoreboard.first_job,
+                      pdev->gpu_id);
 
       if (debug & PANVK_DEBUG_DUMP)
-         pandecode_dump_mappings();
+         pandecode_dump_mappings(pdev->decode_ctx);
    }
 
    if (batch->fragment_job) {
@@ -109,14 +110,14 @@ panvk_queue_submit_batch(struct panvk_queue *queue, struct panvk_batch *batch,
       }
 
       if (debug & PANVK_DEBUG_TRACE)
-         pandecode_jc(batch->fragment_job, pdev->gpu_id);
+         pandecode_jc(pdev->decode_ctx, batch->fragment_job, pdev->gpu_id);
 
       if (debug & PANVK_DEBUG_DUMP)
-         pandecode_dump_mappings();
+         pandecode_dump_mappings(pdev->decode_ctx);
    }
 
    if (debug & PANVK_DEBUG_TRACE)
-      pandecode_next_frame();
+      pandecode_next_frame(0);
 
    batch->issued = true;
 }

src/gallium/drivers/panfrost/pan_context.c
@@ -114,7 +114,7 @@ panfrost_flush(struct pipe_context *pipe, struct pipe_fence_handle **fence,
    }
 
    if (dev->debug & PAN_DBG_TRACE)
-      pandecode_next_frame();
+      pandecode_next_frame(dev->decode_ctx);
 }
 
 static void

src/gallium/drivers/panfrost/pan_job.c
@@ -685,14 +685,14 @@ panfrost_batch_submit_ioctl(struct panfrost_batch *batch,
       drmSyncobjWait(dev->fd, &out_sync, 1, INT64_MAX, 0, NULL);
 
       if (dev->debug & PAN_DBG_TRACE)
-         pandecode_jc(submit.jc, dev->gpu_id);
+         pandecode_jc(dev->decode_ctx, submit.jc, dev->gpu_id);
 
       if (dev->debug & PAN_DBG_DUMP)
-         pandecode_dump_mappings();
+         pandecode_dump_mappings(dev->decode_ctx);
 
       /* Jobs won't be complete if blackhole rendering, that's ok */
       if (!ctx->is_noop && dev->debug & PAN_DBG_SYNC)
-         pandecode_abort_on_fault(submit.jc, dev->gpu_id);
+         pandecode_abort_on_fault(dev->decode_ctx, submit.jc, dev->gpu_id);
    }
 
    return 0;

src/gallium/drivers/panfrost/pan_resource.c
@@ -1004,7 +1004,8 @@ panfrost_ptr_map(struct pipe_context *pctx, struct pipe_resource *resource,
    panfrost_bo_mmap(bo);
 
    if (dev->debug & (PAN_DBG_TRACE | PAN_DBG_SYNC))
-      pandecode_inject_mmap(bo->ptr.gpu, bo->ptr.cpu, bo->size, NULL);
+      pandecode_inject_mmap(dev->decode_ctx, bo->ptr.gpu, bo->ptr.cpu, bo->size,
+                            NULL);
 
    /* Upgrade writes to uninitialized ranges to UNSYNCHRONIZED */
    if ((usage & PIPE_MAP_WRITE) && resource->target == PIPE_BUFFER &&

src/panfrost/lib/genxml/decode.c
@@ -45,11 +45,12 @@
  * larger FBD */
 
 static void
-pandecode_midgard_tiler_descriptor(const struct mali_tiler_context_packed *tp,
+pandecode_midgard_tiler_descriptor(struct pandecode_context *ctx,
+                                   const struct mali_tiler_context_packed *tp,
                                    const struct mali_tiler_weights_packed *wp)
 {
    pan_unpack(tp, TILER_CONTEXT, t);
-   DUMP_UNPACKED(TILER_CONTEXT, t, "Tiler:\n");
+   DUMP_UNPACKED(ctx, TILER_CONTEXT, t, "Tiler:\n");
 
    /* We've never seen weights used in practice, but they exist */
    pan_unpack(wp, TILER_WEIGHTS, w);
@@ -65,55 +66,58 @@ pandecode_midgard_tiler_descriptor(const struct mali_tiler_context_packed *tp,
    nonzero_weights |= w.weight7 != 0x0;
 
    if (nonzero_weights)
-      DUMP_UNPACKED(TILER_WEIGHTS, w, "Tiler Weights:\n");
+      DUMP_UNPACKED(ctx, TILER_WEIGHTS, w, "Tiler Weights:\n");
 }
 #endif
 
 #if PAN_ARCH >= 5
 static void
-pandecode_render_target(uint64_t gpu_va, unsigned gpu_id,
+pandecode_render_target(struct pandecode_context *ctx, uint64_t gpu_va,
+                        unsigned gpu_id,
                         const struct MALI_FRAMEBUFFER_PARAMETERS *fb)
 {
-   pandecode_log("Color Render Targets @%" PRIx64 ":\n", gpu_va);
-   pandecode_indent++;
+   pandecode_log(ctx, "Color Render Targets @%" PRIx64 ":\n", gpu_va);
+   ctx->indent++;
 
    for (int i = 0; i < (fb->render_target_count); i++) {
       mali_ptr rt_va = gpu_va + i * pan_size(RENDER_TARGET);
       const struct mali_render_target_packed *PANDECODE_PTR_VAR(
-         rtp, (mali_ptr)rt_va);
-      DUMP_CL(RENDER_TARGET, rtp, "Color Render Target %d:\n", i);
+         ctx, rtp, (mali_ptr)rt_va);
+      DUMP_CL(ctx, RENDER_TARGET, rtp, "Color Render Target %d:\n", i);
    }
 
-   pandecode_indent--;
-   pandecode_log("\n");
+   ctx->indent--;
+   pandecode_log(ctx, "\n");
 }
 #endif
 
 #if PAN_ARCH >= 6
 static void
-pandecode_sample_locations(const void *fb)
+pandecode_sample_locations(struct pandecode_context *ctx, const void *fb)
 {
    pan_section_unpack(fb, FRAMEBUFFER, PARAMETERS, params);
 
-   const u16 *PANDECODE_PTR_VAR(samples, params.sample_locations);
+   const u16 *PANDECODE_PTR_VAR(ctx, samples, params.sample_locations);
 
-   pandecode_log("Sample locations @%" PRIx64 ":\n", params.sample_locations);
+   pandecode_log(ctx, "Sample locations @%" PRIx64 ":\n",
+                 params.sample_locations);
    for (int i = 0; i < 33; i++) {
-      pandecode_log("  (%d, %d),\n", samples[2 * i] - 128,
+      pandecode_log(ctx, "  (%d, %d),\n", samples[2 * i] - 128,
                     samples[2 * i + 1] - 128);
    }
 }
 #endif
 
 struct pandecode_fbd
-GENX(pandecode_fbd)(uint64_t gpu_va, bool is_fragment, unsigned gpu_id)
+GENX(pandecode_fbd)(struct pandecode_context *ctx, uint64_t gpu_va,
+                    bool is_fragment, unsigned gpu_id)
 {
-   const void *PANDECODE_PTR_VAR(fb, (mali_ptr)gpu_va);
+   const void *PANDECODE_PTR_VAR(ctx, fb, (mali_ptr)gpu_va);
    pan_section_unpack(fb, FRAMEBUFFER, PARAMETERS, params);
-   DUMP_UNPACKED(FRAMEBUFFER_PARAMETERS, params, "Parameters:\n");
+   DUMP_UNPACKED(ctx, FRAMEBUFFER_PARAMETERS, params, "Parameters:\n");
 
 #if PAN_ARCH >= 6
-   pandecode_sample_locations(fb);
+   pandecode_sample_locations(ctx, fb);
 
    unsigned dcd_size = pan_size(DRAW);
    unsigned job_type_param = 0;
@@ -123,64 +127,64 @@ GENX(pandecode_fbd)(uint64_t gpu_va, bool is_fragment, unsigned gpu_id)
 #endif
 
    if (params.pre_frame_0 != MALI_PRE_POST_FRAME_SHADER_MODE_NEVER) {
-      const void *PANDECODE_PTR_VAR(dcd,
+      const void *PANDECODE_PTR_VAR(ctx, dcd,
                                     params.frame_shader_dcds + (0 * dcd_size));
       pan_unpack(dcd, DRAW, draw);
-      pandecode_log("Pre frame 0 @%" PRIx64 " (mode=%d):\n",
+      pandecode_log(ctx, "Pre frame 0 @%" PRIx64 " (mode=%d):\n",
                     params.frame_shader_dcds, params.pre_frame_0);
-      GENX(pandecode_dcd)(&draw, job_type_param, gpu_id);
+      GENX(pandecode_dcd)(ctx, &draw, job_type_param, gpu_id);
    }
 
    if (params.pre_frame_1 != MALI_PRE_POST_FRAME_SHADER_MODE_NEVER) {
-      const void *PANDECODE_PTR_VAR(dcd,
+      const void *PANDECODE_PTR_VAR(ctx, dcd,
                                     params.frame_shader_dcds + (1 * dcd_size));
       pan_unpack(dcd, DRAW, draw);
-      pandecode_log("Pre frame 1 @%" PRIx64 ":\n",
+      pandecode_log(ctx, "Pre frame 1 @%" PRIx64 ":\n",
                     params.frame_shader_dcds + (1 * dcd_size));
-      GENX(pandecode_dcd)(&draw, job_type_param, gpu_id);
+      GENX(pandecode_dcd)(ctx, &draw, job_type_param, gpu_id);
    }
 
    if (params.post_frame != MALI_PRE_POST_FRAME_SHADER_MODE_NEVER) {
-      const void *PANDECODE_PTR_VAR(dcd,
+      const void *PANDECODE_PTR_VAR(ctx, dcd,
                                     params.frame_shader_dcds + (2 * dcd_size));
       pan_unpack(dcd, DRAW, draw);
-      pandecode_log("Post frame:\n");
-      GENX(pandecode_dcd)(&draw, job_type_param, gpu_id);
+      pandecode_log(ctx, "Post frame:\n");
+      GENX(pandecode_dcd)(ctx, &draw, job_type_param, gpu_id);
    }
 #else
-   DUMP_SECTION(FRAMEBUFFER, LOCAL_STORAGE, fb, "Local Storage:\n");
+   DUMP_SECTION(ctx, FRAMEBUFFER, LOCAL_STORAGE, fb, "Local Storage:\n");
 
    const void *t = pan_section_ptr(fb, FRAMEBUFFER, TILER);
    const void *w = pan_section_ptr(fb, FRAMEBUFFER, TILER_WEIGHTS);
-   pandecode_midgard_tiler_descriptor(t, w);
+   pandecode_midgard_tiler_descriptor(ctx, t, w);
 #endif
 
-   pandecode_log("Framebuffer @%" PRIx64 ":\n", gpu_va);
-   pandecode_indent++;
+   pandecode_log(ctx, "Framebuffer @%" PRIx64 ":\n", gpu_va);
+   ctx->indent++;
 
-   DUMP_UNPACKED(FRAMEBUFFER_PARAMETERS, params, "Parameters:\n");
+   DUMP_UNPACKED(ctx, FRAMEBUFFER_PARAMETERS, params, "Parameters:\n");
 #if PAN_ARCH >= 6
    if (params.tiler)
-      GENX(pandecode_tiler)(params.tiler, gpu_id);
+      GENX(pandecode_tiler)(ctx, params.tiler, gpu_id);
 #endif
 
-   pandecode_indent--;
-   pandecode_log("\n");
+   ctx->indent--;
+   pandecode_log(ctx, "\n");
 
 #if PAN_ARCH >= 5
    gpu_va += pan_size(FRAMEBUFFER);
 
    if (params.has_zs_crc_extension) {
       const struct mali_zs_crc_extension_packed *PANDECODE_PTR_VAR(
-         zs_crc, (mali_ptr)gpu_va);
-      DUMP_CL(ZS_CRC_EXTENSION, zs_crc, "ZS CRC Extension:\n");
-      pandecode_log("\n");
+         ctx, zs_crc, (mali_ptr)gpu_va);
+      DUMP_CL(ctx, ZS_CRC_EXTENSION, zs_crc, "ZS CRC Extension:\n");
+      pandecode_log(ctx, "\n");
 
       gpu_va += pan_size(ZS_CRC_EXTENSION);
    }
 
    if (is_fragment)
-      pandecode_render_target(gpu_va, gpu_id, &params);
+      pandecode_render_target(ctx, gpu_va, gpu_id, &params);
 
    return (struct pandecode_fbd){
       .rt_count = params.render_target_count,
@@ -201,10 +205,11 @@ GENX(pandecode_fbd)(uint64_t gpu_va, bool is_fragment, unsigned gpu_id)
 
 #if PAN_ARCH >= 5
 mali_ptr
-GENX(pandecode_blend)(void *descs, int rt_no, mali_ptr frag_shader)
+GENX(pandecode_blend)(struct pandecode_context *ctx, void *descs, int rt_no,
+                      mali_ptr frag_shader)
 {
    pan_unpack(descs + (rt_no * pan_size(BLEND)), BLEND, b);
-   DUMP_UNPACKED(BLEND, b, "Blend RT %d:\n", rt_no);
+   DUMP_UNPACKED(ctx, BLEND, b, "Blend RT %d:\n", rt_no);
 #if PAN_ARCH >= 6
    if (b.internal.mode != MALI_BLEND_MODE_SHADER)
       return 0;
@@ -231,7 +236,8 @@ panfrost_is_yuv_format(uint32_t packed)
 }
 
 static void
-pandecode_texture_payload(mali_ptr payload, const struct MALI_TEXTURE *tex)
+pandecode_texture_payload(struct pandecode_context *ctx, mali_ptr payload,
+                          const struct MALI_TEXTURE *tex)
 {
    unsigned nr_samples =
       tex->dimension == MALI_TEXTURE_DIMENSION_3D ? 1 : tex->sample_count;
@@ -257,8 +263,8 @@ pandecode_texture_payload(mali_ptr payload, const struct MALI_TEXTURE *tex)
 #define PANDECODE_EMIT_TEX_PAYLOAD_DESC(T, msg)                                \
    for (int i = 0; i < bitmap_count; ++i) {                                    \
       uint64_t addr = payload + pan_size(T) * i;                               \
-      pan_unpack(PANDECODE_PTR(addr, void), T, s);                             \
-      DUMP_UNPACKED(T, s, msg " @%" PRIx64 ":\n", addr)                        \
+      pan_unpack(PANDECODE_PTR(ctx, addr, void), T, s);                        \
+      DUMP_UNPACKED(ctx, T, s, msg " @%" PRIx64 ":\n", addr)                   \
    }
 
 #if PAN_ARCH <= 5
@@ -277,7 +283,7 @@ pandecode_texture_payload(mali_ptr payload, const struct MALI_TEXTURE *tex)
                                       "Surface With Stride");
       break;
    default:
-      fprintf(pandecode_dump_stream, "Unknown surface descriptor type %X\n",
+      fprintf(ctx->dump_stream, "Unknown surface descriptor type %X\n",
               tex->surface_type);
       break;
    }
@@ -299,25 +305,26 @@ pandecode_texture_payload(mali_ptr payload, const struct MALI_TEXTURE *tex)
 
 #if PAN_ARCH <= 5
 void
-GENX(pandecode_texture)(mali_ptr u, unsigned tex)
+GENX(pandecode_texture)(struct pandecode_context *ctx, mali_ptr u, unsigned tex)
 {
-   const uint8_t *cl = pandecode_fetch_gpu_mem(u, pan_size(TEXTURE));
+   const uint8_t *cl = pandecode_fetch_gpu_mem(ctx, u, pan_size(TEXTURE));
 
    pan_unpack(cl, TEXTURE, temp);
-   DUMP_UNPACKED(TEXTURE, temp, "Texture:\n")
+   DUMP_UNPACKED(ctx, TEXTURE, temp, "Texture:\n")
 
-   pandecode_indent++;
-   pandecode_texture_payload(u + pan_size(TEXTURE), &temp);
-   pandecode_indent--;
+   ctx->indent++;
+   pandecode_texture_payload(ctx, u + pan_size(TEXTURE), &temp);
+   ctx->indent--;
 }
 #else
 void
-GENX(pandecode_texture)(const void *cl, unsigned tex)
+GENX(pandecode_texture)(struct pandecode_context *ctx, const void *cl,
+                        unsigned tex)
 {
    pan_unpack(cl, TEXTURE, temp);
-   DUMP_UNPACKED(TEXTURE, temp, "Texture:\n")
+   DUMP_UNPACKED(ctx, TEXTURE, temp, "Texture:\n")
 
-   pandecode_indent++;
+   ctx->indent++;
 
 #if PAN_ARCH >= 9
    int plane_count = temp.levels * temp.array_size;
@@ -327,66 +334,70 @@ GENX(pandecode_texture)(const void *cl, unsigned tex)
       plane_count *= 6;
 
    for (unsigned i = 0; i < plane_count; ++i)
-      DUMP_ADDR(PLANE, temp.surfaces + i * pan_size(PLANE), "Plane %u:\n", i);
+      DUMP_ADDR(ctx, PLANE, temp.surfaces + i * pan_size(PLANE), "Plane %u:\n",
+                i);
 #else
-   pandecode_texture_payload(temp.surfaces, &temp);
+   pandecode_texture_payload(ctx, temp.surfaces, &temp);
 #endif
-   pandecode_indent--;
+   ctx->indent--;
 }
 #endif
 
 #if PAN_ARCH >= 6
 void
-GENX(pandecode_tiler)(mali_ptr gpu_va, unsigned gpu_id)
+GENX(pandecode_tiler)(struct pandecode_context *ctx, mali_ptr gpu_va,
+                      unsigned gpu_id)
 {
-   pan_unpack(PANDECODE_PTR(gpu_va, void), TILER_CONTEXT, t);
+   pan_unpack(PANDECODE_PTR(ctx, gpu_va, void), TILER_CONTEXT, t);
 
    if (t.heap) {
-      pan_unpack(PANDECODE_PTR(t.heap, void), TILER_HEAP, h);
-      DUMP_UNPACKED(TILER_HEAP, h, "Tiler Heap:\n");
+      pan_unpack(PANDECODE_PTR(ctx, t.heap, void), TILER_HEAP, h);
+      DUMP_UNPACKED(ctx, TILER_HEAP, h, "Tiler Heap:\n");
    }
 
-   DUMP_UNPACKED(TILER_CONTEXT, t, "Tiler Context @%" PRIx64 ":\n", gpu_va);
+   DUMP_UNPACKED(ctx, TILER_CONTEXT, t, "Tiler Context @%" PRIx64 ":\n",
+                 gpu_va);
 }
 #endif
 
 #if PAN_ARCH >= 9
 void
-GENX(pandecode_fau)(mali_ptr addr, unsigned count, const char *name)
+GENX(pandecode_fau)(struct pandecode_context *ctx, mali_ptr addr,
+                    unsigned count, const char *name)
 {
    if (count == 0)
       return;
 
-   const uint32_t *PANDECODE_PTR_VAR(raw, addr);
+   const uint32_t *PANDECODE_PTR_VAR(ctx, raw, addr);
 
-   pandecode_validate_buffer(addr, count * 8);
+   pandecode_validate_buffer(ctx, addr, count * 8);
 
-   fprintf(pandecode_dump_stream, "%s @%" PRIx64 ":\n", name, addr);
+   fprintf(ctx->dump_stream, "%s @%" PRIx64 ":\n", name, addr);
    for (unsigned i = 0; i < count; ++i) {
-      fprintf(pandecode_dump_stream, "  %08X %08X\n", raw[2 * i],
-              raw[2 * i + 1]);
+      fprintf(ctx->dump_stream, "  %08X %08X\n", raw[2 * i], raw[2 * i + 1]);
    }
-   fprintf(pandecode_dump_stream, "\n");
+   fprintf(ctx->dump_stream, "\n");
 }
 
 mali_ptr
-GENX(pandecode_shader)(mali_ptr addr, const char *label, unsigned gpu_id)
+GENX(pandecode_shader)(struct pandecode_context *ctx, mali_ptr addr,
+                       const char *label, unsigned gpu_id)
 {
-   MAP_ADDR(SHADER_PROGRAM, addr, cl);
+   MAP_ADDR(ctx, SHADER_PROGRAM, addr, cl);
    pan_unpack(cl, SHADER_PROGRAM, desc);
 
    assert(desc.type == 8);
 
-   DUMP_UNPACKED(SHADER_PROGRAM, desc, "%s Shader @%" PRIx64 ":\n", label,
+   DUMP_UNPACKED(ctx, SHADER_PROGRAM, desc, "%s Shader @%" PRIx64 ":\n", label,
                  addr);
-   pandecode_shader_disassemble(desc.binary, gpu_id);
+   pandecode_shader_disassemble(ctx, desc.binary, gpu_id);
    return desc.binary;
 }
 
 static void
-pandecode_resources(mali_ptr addr, unsigned size)
+pandecode_resources(struct pandecode_context *ctx, mali_ptr addr, unsigned size)
 {
-   const uint8_t *cl = pandecode_fetch_gpu_mem(addr, size);
+   const uint8_t *cl = pandecode_fetch_gpu_mem(ctx, addr, size);
    assert((size % 0x20) == 0);
 
    for (unsigned i = 0; i < size; i += 0x20) {
@@ -394,96 +405,101 @@ pandecode_resources(mali_ptr addr, unsigned size)
 
       switch (type) {
       case MALI_DESCRIPTOR_TYPE_SAMPLER:
-         DUMP_CL(SAMPLER, cl + i, "Sampler @%" PRIx64 ":\n", addr + i);
+         DUMP_CL(ctx, SAMPLER, cl + i, "Sampler @%" PRIx64 ":\n", addr + i);
          break;
       case MALI_DESCRIPTOR_TYPE_TEXTURE:
-         pandecode_log("Texture @%" PRIx64 "\n", addr + i);
-         GENX(pandecode_texture)(cl + i, i);
+         pandecode_log(ctx, "Texture @%" PRIx64 "\n", addr + i);
+         GENX(pandecode_texture)(ctx, cl + i, i);
          break;
       case MALI_DESCRIPTOR_TYPE_ATTRIBUTE:
-         DUMP_CL(ATTRIBUTE, cl + i, "Attribute @%" PRIx64 ":\n", addr + i);
+         DUMP_CL(ctx, ATTRIBUTE, cl + i, "Attribute @%" PRIx64 ":\n", addr + i);
          break;
       case MALI_DESCRIPTOR_TYPE_BUFFER:
-         DUMP_CL(BUFFER, cl + i, "Buffer @%" PRIx64 ":\n", addr + i);
+         DUMP_CL(ctx, BUFFER, cl + i, "Buffer @%" PRIx64 ":\n", addr + i);
          break;
       default:
-         fprintf(pandecode_dump_stream, "Unknown descriptor type %X\n", type);
+         fprintf(ctx->dump_stream, "Unknown descriptor type %X\n", type);
          break;
       }
    }
 }
 
 void
-GENX(pandecode_resource_tables)(mali_ptr addr, const char *label)
+GENX(pandecode_resource_tables)(struct pandecode_context *ctx, mali_ptr addr,
+                                const char *label)
 {
    unsigned count = addr & 0x3F;
    addr = addr & ~0x3F;
 
    const uint8_t *cl =
-      pandecode_fetch_gpu_mem(addr, MALI_RESOURCE_LENGTH * count);
+      pandecode_fetch_gpu_mem(ctx, addr, MALI_RESOURCE_LENGTH * count);
 
    for (unsigned i = 0; i < count; ++i) {
       pan_unpack(cl + i * MALI_RESOURCE_LENGTH, RESOURCE, entry);
-      DUMP_UNPACKED(RESOURCE, entry, "Entry %u @%" PRIx64 ":\n", i,
+      DUMP_UNPACKED(ctx, RESOURCE, entry, "Entry %u @%" PRIx64 ":\n", i,
                     addr + i * MALI_RESOURCE_LENGTH);
 
-      pandecode_indent += 2;
+      ctx->indent += 2;
       if (entry.address)
-         pandecode_resources(entry.address, entry.size);
-      pandecode_indent -= 2;
+         pandecode_resources(ctx, entry.address, entry.size);
+      ctx->indent -= 2;
    }
 }
 
 void
-GENX(pandecode_depth_stencil)(mali_ptr addr)
+GENX(pandecode_depth_stencil)(struct pandecode_context *ctx, mali_ptr addr)
 {
-   MAP_ADDR(DEPTH_STENCIL, addr, cl);
+   MAP_ADDR(ctx, DEPTH_STENCIL, addr, cl);
    pan_unpack(cl, DEPTH_STENCIL, desc);
-   DUMP_UNPACKED(DEPTH_STENCIL, desc, "Depth/stencil");
+   DUMP_UNPACKED(ctx, DEPTH_STENCIL, desc, "Depth/stencil");
 }
 
 void
-GENX(pandecode_shader_environment)(const struct MALI_SHADER_ENVIRONMENT *p,
+GENX(pandecode_shader_environment)(struct pandecode_context *ctx,
+                                   const struct MALI_SHADER_ENVIRONMENT *p,
                                    unsigned gpu_id)
 {
    if (p->shader)
-      GENX(pandecode_shader)(p->shader, "Shader", gpu_id);
+      GENX(pandecode_shader)(ctx, p->shader, "Shader", gpu_id);
 
    if (p->resources)
-      GENX(pandecode_resource_tables)(p->resources, "Resources");
+      GENX(pandecode_resource_tables)(ctx, p->resources, "Resources");
 
    if (p->thread_storage)
-      DUMP_ADDR(LOCAL_STORAGE, p->thread_storage, "Local Storage:\n");
+      DUMP_ADDR(ctx, LOCAL_STORAGE, p->thread_storage, "Local Storage:\n");
 
    if (p->fau)
-      GENX(pandecode_fau)(p->fau, p->fau_count, "FAU");
+      GENX(pandecode_fau)(ctx, p->fau, p->fau_count, "FAU");
 }
 
 void
-GENX(pandecode_blend_descs)(mali_ptr blend, unsigned count,
-                            mali_ptr frag_shader, unsigned gpu_id)
+GENX(pandecode_blend_descs)(struct pandecode_context *ctx, mali_ptr blend,
+                            unsigned count, mali_ptr frag_shader,
+                            unsigned gpu_id)
 {
    for (unsigned i = 0; i < count; ++i) {
-      struct mali_blend_packed *PANDECODE_PTR_VAR(blend_descs, blend);
+      struct mali_blend_packed *PANDECODE_PTR_VAR(ctx, blend_descs, blend);
 
       mali_ptr blend_shader =
-         GENX(pandecode_blend)(blend_descs, i, frag_shader);
+         GENX(pandecode_blend)(ctx, blend_descs, i, frag_shader);
       if (blend_shader) {
-         fprintf(pandecode_dump_stream, "Blend shader %u @%" PRIx64 "", i,
+         fprintf(ctx->dump_stream, "Blend shader %u @%" PRIx64 "", i,
                  blend_shader);
-         pandecode_shader_disassemble(blend_shader, gpu_id);
+         pandecode_shader_disassemble(ctx, blend_shader, gpu_id);
       }
    }
 }
 
 void
-GENX(pandecode_dcd)(const struct MALI_DRAW *p, unsigned unused, unsigned gpu_id)
+GENX(pandecode_dcd)(struct pandecode_context *ctx, const struct MALI_DRAW *p,
+                    unsigned unused, unsigned gpu_id)
 {
    mali_ptr frag_shader = 0;
 
-   GENX(pandecode_depth_stencil)(p->depth_stencil);
-   GENX(pandecode_blend_descs)(p->blend, p->blend_count, frag_shader, gpu_id);
-   GENX(pandecode_shader_environment)(&p->shader, gpu_id);
-   DUMP_UNPACKED(DRAW, *p, "Draw:\n");
+   GENX(pandecode_depth_stencil)(ctx, p->depth_stencil);
+   GENX(pandecode_blend_descs)
+   (ctx, p->blend, p->blend_count, frag_shader, gpu_id);
+   GENX(pandecode_shader_environment)(ctx, &p->shader, gpu_id);
+   DUMP_UNPACKED(ctx, DRAW, *p, "Draw:\n");
 }
 #endif

ThatMG393/panfrost-gallium-mesa

commit 3227b4d207839139cb0d23dfd70c8380b22500b2

panvk: Fix tracing
pandecode_next_frame() take a decode context. Passing NULL leads to a
NULL deref.

Fixes: 56be9a55be03 ("pan/decode: handle more than one panfrost_device")
Cc: stable
Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>

src/panfrost/vulkan/panvk_vX_device.c
@@ -121,7 +121,7 @@ panvk_queue_submit_batch(struct panvk_queue *queue, struct panvk_batch *batch,
    }
 
    if (debug & PANVK_DEBUG_TRACE)
-      pandecode_next_frame(0);
+      pandecode_next_frame(pdev->decode_ctx);
 
    batch->issued = true;
 }