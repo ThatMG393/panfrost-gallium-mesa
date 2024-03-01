299b97bb9d55e89d3c9b2f6097fb0c7c8e32192b
SolDev69 <40839581+SolDev69@users.noreply.github.com>
2024-03-01 13:00:17 -0500

    Test GPU redirection support
    For Gx15 Malis and newer

--- src/gallium/drivers/panfrost/pan_screen.c
+++ src/gallium/drivers/panfrost/pan_screen.c
@@ -893,10 +893,8 @@ panfrost_create_screen(int fd, struct renderonly *ro)
                 panfrost_cmdstream_screen_init_v7(screen);
         else if (dev->arch == 9)
                 panfrost_cmdstream_screen_init_v9(screen);
-        else if (dev->arch == 10)
+        else // pretend we are running on v10 hardware even if we are on v11
                 panfrost_cmdstream_screen_init_v10(screen);
-        else
-                unreachable("Unhandled architecture major");
 
         return &screen->base;
 }
